a155ce0fb59711c94fccd08ebfeee3ce3cf88748
Khalifa <m_khalifa@tss-hr420-dm17.local>
2019-01-28 16:24:38 -0500

    js

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,16 +1,37 @@
 (() => {
-	// select the elements that u want to work with
-	let theButton = document.querySelector("#buttonHolder img");
-	//window.addEventListener("load", changeHeaderText);
+	// variables always come first
+	// set up the puzzle pieces and boards
+	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
-	// fnctions go in the middle
-	function changeHeaderText(){
-		document.querySelector("h1").textContent = "Wait for it...";
-		document.querySelector("p").textContent = "Aha!";
+	let piecesBoard = document.querySelector(".puzzle-pieces"),	
+		puzzleBoard = document.querySelector(".puzzle-board"),
+		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
+
+		//functions go in the middle
+	function createPuzzlePieces(pictureIndex) {
+		//generate puzzle pieces for the left hand side
+		//debugger;
+		pieces.forEach((piece, index) => {
+			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
+			
+			piecesBoard.innerHTML += newPuzzlePiece;
+		});
+
+		puzzleBoard.style.backgroundImage = `url(images/backGround${pictureIndex}.jpg)`;
 	}
 
-	//event handing goes at teh bottom
-	theButton.addEventListener("click", changeHeaderText);
-	// set up the puzzle pieces and boards
-	
+	function resetPuzzlePieces() {
+		// empty the thumbnail container
+		piecesBoard.innerHTML = " ";
+		createPuzzlePieces(this.dataset.puzzleref);
+	}
+
+	// event handling down here
+	puzzleSelectors.forEach(puzzle => puzzle.addEventListener("click", resetPuzzlePieces));
+	createPuzzlePieces(0);
+
+
+
+
+
 })();
