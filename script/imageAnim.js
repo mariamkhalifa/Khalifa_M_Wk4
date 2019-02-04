3f8278375321789fd15930f0021695a7d5fd6156
mariamkhalifa <m_khalifa@fanhsaweonline.ca>
2019-02-04 16:36:09 -0500

    drag and drop functionality

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -7,18 +7,51 @@
 		puzzleBoard = document.querySelector(".puzzle-board"),
 		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
 
+		let dropZones = document.querySelectorAll(".drop-zone");
+
 		//functions go in the middle
 	function createPuzzlePieces(pictureIndex) {
 		//generate puzzle pieces for the left hand side
 		//debugger;
 		pieces.forEach((piece, index) => {
-			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
+			let newPuzzlePiece = `<img draggable id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
 			
 			piecesBoard.innerHTML += newPuzzlePiece;
 		});
 
 		puzzleBoard.style.backgroundImage = `url(images/backGround${pictureIndex}.jpg)`;
+
+		initDrag();
+
 	}
+
+
+	// drag and drop functionality goes here
+	function initDrag() {
+		piecesBoard.querySelectorAll("img").forEach(img=> {
+			img.addEventListener("dragstart", function(e) {
+				console.log("draggin...");
+
+				e.dataTransfer.setData("text/plain",this.id);
+			});
+		});
+	}
+
+	//handle drag over and drop
+	dropZones.forEach(zone => {
+		zone.addEventListener("dragover", function(e) {
+			e.preventDefault();
+			console.log("you dragged over me!");
+		});
+
+		zone.addEventListener("drop", function(e) {
+			e.preventDefault();
+			console.log("you dropped sumpin on me");
+
+			let peice = e.dataTransfer.getData("text/plain");
+			e.target.appendChild(document.querySelector(`#${peice}`));
+		});
+	})
 
 	function resetPuzzlePieces() {
 		// empty the thumbnail container
